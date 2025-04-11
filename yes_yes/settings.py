b8cc28168a4a7ea395be661d4f5f471da1410c75
popurisambasivarao.ssr@gmail.com <popurisambasivarao.ssr@gmail.com>
2025-04-11 11:34:02 +0530

    update

--- yes_yes/settings.py
+++ yes_yes/settings.py
@@ -78,9 +78,17 @@ WSGI_APPLICATION = 'yes_yes.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'yesyes',
+        'USER': 'yesyandyes',
+        'PASSWORD': 'yes_and_yes',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
